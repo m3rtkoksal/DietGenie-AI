d380ddf5c987cc368bb83fcdcb0c65313439936c
Mert Köksal <129754261+FinagoMertK@users.noreply.github.com>
2024-08-15 23:51:22 +0300

    mask did not work in validation field

--- DietGenie AI/Utilities/CUIValidationField.swift
+++ DietGenie AI/Utilities/CUIValidationField.swift
@@ -19,41 +19,39 @@ struct CUIValidationField: View {
     @Binding var showPrompt: Bool
     
     var body: some View {
-        LinearGradient(gradient: Gradient(colors: [.red, .blue, .teal]), startPoint: .leading, endPoint: .trailing)
-            .frame(height: 60)
-            .mask(VStack(alignment: .leading) {
-                HStack {
-                        CUIDefaultTextField(
-                            text: $text,
-                            placeholder: placeholder,
-                            fontColor: .teal)
-                        .padding(.leading, 3)
-                        .frame(height: 50)
-                        .frame(maxWidth: UIScreen.screenWidth / 1.1)
-                        
-                    
-                }
-                .onChange(of: text) { newValue in
-                    isCriteriaValid = false
-                    isNotValid = false
-                    showPrompt = false
-                }
+        VStack(alignment: .leading) {
+            HStack {
+                CUIDefaultTextField(
+                    text: $text,
+                    placeholder: placeholder,
+                    fontColor: .teal)
+                .padding(.leading, 3)
+                .frame(height: 50)
                 .frame(maxWidth: UIScreen.screenWidth / 1.1)
-                .background(.clear)
-                .cornerRadius(10)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 8)
-                        .strokeBorder(isNotValid ? Color.red : .lightGray200, lineWidth: 1)
-                )
                 
                 
-                if  showPrompt && willShowPrompt {
-                    Text(prompt)
-                        .fixedSize(horizontal: false, vertical: true)
-                        .font(.footnote)
-                        .foregroundColor(Color.red)
-                }
-            }.padding(.vertical, 5))
-      
+            }
+            .onChange(of: text) { newValue in
+                isCriteriaValid = false
+                isNotValid = false
+                showPrompt = false
+            }
+            .frame(maxWidth: UIScreen.screenWidth / 1.1)
+            .background(.clear)
+            .cornerRadius(10)
+            .overlay(
+                RoundedRectangle(cornerRadius: 8)
+                    .strokeBorder(isNotValid ? Color.red : .lightGray200, lineWidth: 1)
+            )
+            
+            
+            if  showPrompt && willShowPrompt {
+                Text(prompt)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .font(.footnote)
+                    .foregroundColor(Color.red)
+            }
+        }.padding(.vertical, 5)
+        
     }
 }
